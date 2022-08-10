d8a76fd5ba9e37a386dd8dc6eca8d6bccc404566
sntdshrly <71547739+sntdshrly@users.noreply.github.com>
2022-08-10 21:56:51 +0700

    Delete iterator_pattern directory

--- iterator_pattern/src/Aggregate.java
+++
@@ -1,6 +0,0 @@
-// Nama Program: Iterator Pattern
-// Nama/NRP: 2072025/Sherly Santiadi
-
-public interface Aggregate{
-    Iterator getIterator();
-}
--- iterator_pattern/src/Iterator.java
+++
@@ -1,7 +0,0 @@
-// Nama Program: Iterator Pattern
-// Nama/NRP: 2072025/Sherly Santiadi
-
-public interface Iterator{
-    boolean hasNext();
-    Object next();
-}
